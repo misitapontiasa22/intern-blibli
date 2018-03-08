3054b63d33062fe91ecc8c6c369950d960fdccef
misitapontiasa22 <misita.pontiasa@binus.ac.id>
2018-03-08 13:55:50 +0700

    coba conflict

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-    System.out.println("Hello World woq woe ongii");
+		System.out.println("Hello World woq woe ongii asd");
 		System.out.println("Hello World!");
 		System.out.println("Git pull ex");
 		System.out.println("Hello World sstttt");
